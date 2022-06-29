22beca0d738a3627e65aa65b7b57c4e2edf750fd
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2022-06-30 00:03:58 +0530

    datatypes

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,16 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello, Ananthu");
+        int myFirstNumber=5;
+//        myFirstnumber+=20;
+        int mySecondNumber=12;
+        int myThirdNumber=6;
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;//23
+        System.out.println(myFirstNumber);
+        System.out.println("myfirstnumber "+myFirstNumber);
+        System.out.println("Total :"+myTotal);
+        int myFinalone=myTotal-1000;
+        System.out.println(myFinalone);
     }
 }
