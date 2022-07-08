e2b23982a4ef1e90fb0fea7ddcd4adbe39b9520e
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2022-07-08 21:16:12 +0530

    integer data type max  and min values

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -3,5 +3,12 @@ package academy.learnprogramming;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hey");
+        int myMinValue=Integer.MIN_VALUE;
+        int myMaxValue=Integer.MAX_VALUE;
+        System.out.println("Integer minimum value=" + myMinValue);
+        System.out.println("Integer maximum value=" + myMaxValue);
+        System.out.println("Busted max value =" + (myMaxValue+1));// Overflow
+        System.out.println("Busted min value =" + (myMinValue-1));// Underflow
+
     }
 }
