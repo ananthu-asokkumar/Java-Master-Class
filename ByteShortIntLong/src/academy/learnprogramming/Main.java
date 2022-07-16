af961ec1dcfdc1e01306213c223ce38eb29782fc
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2022-07-16 22:08:51 +0530

    chore: casting in java

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -10,5 +10,33 @@ public class Main {
         System.out.println("Busted max value =" + (myMaxValue+1));// Overflow
         System.out.println("Busted min value =" + (myMinValue-1));// Underflow
 
+        //byte Datatype
+        byte myMinByteValue=Byte.MIN_VALUE;
+        byte myMaxByteValue=Byte.MAX_VALUE;
+
+        System.out.println("Byte Min value="+myMinByteValue);
+        System.out.println("Byte Max value="+myMaxByteValue);
+
+        //short Datatype
+        short myMinShortValue=Short.MIN_VALUE;
+        short myMaxShortValue=Short.MAX_VALUE;
+
+        System.out.println("Short Min value="+myMinShortValue);
+        System.out.println("Short Max value="+myMaxShortValue);
+
+
+        //long DataType
+
+        long myLongValue=100L;
+        long myMinLongValue=Long.MIN_VALUE;
+        long myMaxLongValue=Long.MAX_VALUE;
+
+        System.out.println("Long Min value="+myMinLongValue);
+        System.out.println("Long Max value="+myMaxLongValue);
+
+        //Casting
+
+        byte myNewByte=(byte) (myMinByteValue/2);
+        short myNewShort=(short) (myMinShortValue/2);
     }
 }
