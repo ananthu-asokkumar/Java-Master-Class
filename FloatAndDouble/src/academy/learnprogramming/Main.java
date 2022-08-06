7454ff871f93a97b7ab02dfe063c0b4f81931e6f
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2022-08-06 14:41:27 +0530

    float and double datatypes

---
+++ FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -0,0 +1,32 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args){
+
+    float myMinFloatValue=Float.MIN_VALUE;;
+    float myMaxFloatvalue=Float.MAX_VALUE;
+        System.out.println("Float min value="+myMinFloatValue);
+        System.out.println("Float max value="+myMaxFloatvalue);
+
+
+        //Double
+
+        double myMinDoubleValue=Double.MIN_VALUE;;
+        double myMaxDoubleValue=Double.MAX_VALUE;
+        System.out.println("Double min value="+myMinDoubleValue);
+        System.out.println("Double max value="+myMaxDoubleValue);
+
+
+        int myIntValue=5/3;
+        float myFloatValue=5f/3f;// default value of decimal is double so better practice is to put f at declarations
+        double myDoubleValue=5d/3d;
+
+        System.out.println("My in t value="+myIntValue);
+        System.out.println("MyFloatValue="+myFloatValue);//7 digits after decimal
+        System.out.println("My double Value="+myDoubleValue);//16 digits
+        // double is more precise than the float
+        
+
+
+    }
+}
