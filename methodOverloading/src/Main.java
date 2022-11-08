ab863c85c61839c84d12142cc0722ce828c21427
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2022-11-08 12:07:49 +0530

    method overloading

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MethodOverloading.calcFeetAndInchesToCentimeters(6,0);
+        MethodOverloading.calcFeetAndInchesToCentimeters(157);
+    }
+}
